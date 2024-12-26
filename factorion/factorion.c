e0015256aa96f04b915928d0ea56fbbd838df68d
Zach Upstone <zu213@bath.ac.uk>
2024-12-26 23:31:54 +0000

    Cleaned up code a bit

--- factorion/factorion.c
+++ factorion/factorion.c
@@ -15,6 +15,9 @@ int factorial(int x){
 }
 
 int convert_to_base(int x, int y){
+    if(y == 10){
+        return x;
+    }
     int count = 0;
     int total = 0;
     while(x){
@@ -23,25 +26,50 @@ int convert_to_base(int x, int y){
         count++;
     }
     return total;
-    
+}
+
+int sum_factorial_digits(int n){
+    int i = n;
+    int running_total = 0;
+    while (n) {
+        running_total += factorial(n % 10);
+        n /= 10;
+        // early return
+        if(running_total > i){
+            break;
+        }
+    }
+    return running_total;
+}
+
+int increment_with_base(int i, int base){
+    i++;
+    if(base == 10){
+        return i;
+    }
+    int temp_i = i;
+    int temp_count = 0;
+    while(1){
+        if((temp_i % 10) - base >= 0){
+            i -= (base * (int)pow(10, temp_count));
+            temp_count += 1;
+            i += (int)pow(10, temp_count);
+            temp_i /= 10;
+        }else{
+            break;
+        }
+    }
+    return i;
 }
 
 int main(){
     // Gather all factorions
-    const int limit = 150;
-    const int collect_number = 10;
+    const int limit = 50000;
+    const int collect_number = 5;
     // working with abses up to 10 to prevent annoying digits
-    const int base = 6;
+    const int base = 10;
 
-    // work out correct array length of calc array so if limit = 100 len = 3  if 99 len = 2
-    int calc_array_len = 0;
-    int temp_limit;
-    while(temp_limit){
-        temp_limit /= 10;
-        calc_array_len++;
-    }
-
-    // initiliase array of 0's
+    // initiliase array of 0's, j is used to see where to add the next index
     int i;
     int j = 0;
     int factorions[collect_number];
@@ -49,47 +77,17 @@ int main(){
         factorions[i] = 0;
     }
 
-    int calc_array[calc_array_len];
-    int n;
-    int temp_count;
-    int running_total;
-    int temp_i;
     i = 1;
-    while (i <= limit) {
-        running_total = 0;
-        n = i;
-        // for every number iterate through its digits and sum up their factorial
-        while (n) {
-            running_total += factorial(n % 10);
-            n /= 10;
-            printf("%i ", running_total);
-            // early return
-            if(running_total > i){
-                break;
-            }
-        }
-
-        if(running_total== convert_to_base(i, base)){
+    while (i <= limit) {        
+        if(sum_factorial_digits(i) == convert_to_base(i, base)){
             factorions[j] = i;
             j++;
         }
 
         // now we need to increase the number realtive to its base
-        i++;
-        temp_i = i;
-        temp_count = 0;
-        while(1){
-            if((temp_i % 10) - base >= 0){
-                i -= (base * (int)pow(10, temp_count));
-                temp_count += 1;
-                i += (int)pow(10, temp_count);
-                temp_i /= 10;
-            }else{
-                break;
-            }
-        }
-        
-    } 
+        i = increment_with_base(i, base);
+    }
+
     for (i = 0; i < collect_number; i++) {
         printf("Factorion: %i \n", factorions[i]);
         fflush( stdout );
